2ca714aa9249c0d63df2961c085a83598a831a3e
adam-gui <145728082+adam-gui@users.noreply.github.com>
2023-09-29 02:34:09 -0300

    completei a base de javascript

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ const dino = document.querySelector('#dino')         //controlar o personagem
 const quadrado = document.querySelector('#quadrado') //controlar o obstáculo
 
 function pular(){   //testar se o personagem possui a classe animar
-    if(personagem.classList != 'animar'){
-        personagem.classList.add('animar')  //se não tiver, adiciona
+    if(dino.classList != 'animar'){
+        dino.classList.add('animar')  //se não tiver, adiciona
     }
 
     setTimeout(function(){
-        personagem.classList.remove('animar')
+        dino.classList.remove('animar')
     }, 500)
 }
+
+var testarColisao = setInterval( function(){
+    
+    var topoDino = parseInt( 
+        window.getComputedStyle(dino).getPropertyValue('top')
+        )
+    var EsquerdaQuadrado = parseInt(
+        window.getComputedStyle(quadrado).getPropertyValue('left')
+       )
+    
+    if(EsquerdaQuadrado < 20 && EsquerdaQuadrado > 0 && topoDino >= 130){
+        quadrado.style.animation = 'none'
+        quadrado.style.display = 'none'
+        alert('Você perdeu!')
+    }
+    
+
+}, 10)
